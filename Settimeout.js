724b272f8e2c5c202bf0398a310ddb0f8e22b2b9
sachin shahi <sachinshahi069@gmail.com>
2026-06-17 14:44:07 +0545

    add:

---
+++ Settimeout.js
@@ -0,0 +1,19 @@
+setTimeout(() => {
+    console.log("hello");
+
+}, 1000);
+setTimeout(() => {
+    console.log("saurabh");
+
+}, 2000);
+setTimeout(() => {
+    console.log("i am sangeeta");
+}, 3000);
+setTimeout(() => {
+    console.log("do you miss me");
+
+}, 4000);
+setTimeout(() => {
+    console.log("i love you saurabh");
+
+}, 5000);
