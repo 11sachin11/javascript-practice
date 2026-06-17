217f49e089fa226b06d5ad12e43eea51afa94a22
sachin shahi <sachinshahi069@gmail.com>
2026-06-17 14:05:31 +0545

    add : initilized first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+let num1 = 5;
+let num2 = 10;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log("Addition:", add(num1, num2));
+console.log("Subtraction:", subtract(num1, num2));
+console.log("Multiplication:", multiply(num1, num2));
